956ca7911778fba1bd3edd536b0e445cb0b249a8
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-04-07 12:50:02 +0000

    Add loaders

--- src/components/GifContainer.tsx
+++ src/components/GifContainer.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../redux/configureStore";
 import { fetchGifs } from "../redux/gif";
+import loads from '../images/ZZ5H.gif'
 
 
 
 const GifContainer = () => {
   const [count, setCount] = useState(10);
     const gifArray = useSelector((state: RootState) => state.gifs.gifs)
+    const [loadedImages, setLoadedImages] = useState<string[]>([]);
     const dispatch = useDispatch<AppDispatch>()
 
  useEffect(() => {
@@ -28,6 +30,10 @@ const handleScroll = () => {
   }
 };
 
+const handleImageLoad = (gifId: string) => {
+  setLoadedImages((prevLoadedImages) => [...prevLoadedImages, gifId]);
+};
+
   
   return (
     <div className="flex flex-wrap gap-4 m-11" >
@@ -37,10 +43,16 @@ const handleScroll = () => {
           className="relative flex-grow w-52 h-52"
           style={{ flexBasis: `${gif.width}px` }}
         >
+          { !loadedImages.includes(gif.id) &&
+               <div className="flex items-center justify-center w-52 h-52">
+                <p>Loading</p><img className="mx-4 w-4 h-4" src={loads} alt=".." />
+               </div>         
+                        }
           <img
             src={gif.image}
             alt={gif.title}
             style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            onLoad={() => handleImageLoad(gif.id)}
           />
           <span className="absolute bottom-1.5 text-white">{gif.title}</span>
         </div>
